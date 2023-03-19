d6ea3a197bac1cdf4f3563333105bd5937d413cf
Vincent Albertsson <albertsson.vincent@gmail.com>
2023-03-19 16:25:20 +0100

    Next and previous post buttons working

--- components/PostCard.js
+++ components/PostCard.js
@@ -12,11 +12,11 @@ export default function PostCard(props) {
     return (
         <>
             <Link href={`/blog/${slug}`} className="">
-                <div className="mb-4 p-4 rounded-lg border border-gray-300 dark:border-none bg-gray-100 dark:bg-[#191B1F] text-center shadow-lg hover:shadow-xl dark:shadow-none">
-                    <h1 className="text-lg mb-2">
-                        {title} |{' '}
+                <div className="mb-4 p-4 rounded-lg border border-gray-300 dark:border-none bg-gray-100 dark:bg-[#191B1F] text-center shadow-lg hover:shadow-xl dark:shadow-none dark:hover:opacity-80">
+                    <h1 className="text-lg">{title}</h1>{' '}
+                    <p className="mb-2 text-xs">
                         {new Date(date).toLocaleDateString('en-US', options)}
-                    </h1>
+                    </p>
                     <p className="line-clamp-3 text-xs">{description}</p>
                 </div>
             </Link>
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,27 +1,57 @@
 import fs from 'fs'
 import matter from 'gray-matter'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
 // The page for each post
-export default function Post({ frontmatter, content }) {
+export default function Post({ frontmatter, content, adjacentPosts }) {
     const { title, description, category, date, postNum, tags } = frontmatter
+    const [nextPost, setNextPost] = useState('')
+    const [previousPost, setPreviousPost] = useState('')
     const options = {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
     }
 
+    useEffect(() => {
+        if (adjacentPosts.length == 1) {
+            if (postNum == 1) {
+                setNextPost(adjacentPosts[0])
+                setPreviousPost('')
+            } else {
+                setNextPost('')
+                setPreviousPost(adjacentPosts[0])
+            }
+        } else {
+            setNextPost(adjacentPosts[0])
+            setPreviousPost(adjacentPosts[1])
+        }
+    }, [adjacentPosts, postNum])
+
     return (
         <div>
-            <h1 className="text-2xl mt-10 text-center underline">{title}</h1>
+            <h1 className="text-2xl mt-10 text-center">{title}</h1>
             <p className="text-center">
                 {new Date(date).toLocaleDateString('en-US', options)}
             </p>
-            <ReactMarkdown className="mt-8">{content}</ReactMarkdown>
-            <div className="items-center justify-center flex gap-5 mt-8">
-                <Link href="/">Previous Post</Link>
-                <Link href="/">Next Post</Link>
+            <ReactMarkdown className="mt-8 leading-relaxed">
+                {content}
+            </ReactMarkdown>
+            <div className="justify-center flex gap-1 mt-8">
+                <Link
+                    href={`/blog/${previousPost}`}
+                    className="py-1 px-1.5 rounded-md text-xs border border-gray-300 text-black shadow-md dark:border-none dark:bg-gray-300 dark:shadow-none hover:shadow-lg dark:hover:opacity-70 duration-150"
+                >
+                    Previous Post
+                </Link>
+                <Link
+                    href={`/blog/${nextPost}`}
+                    className="py-1 px-1.5 rounded-md text-xs border border-gray-300 text-black shadow-md dark:border-none dark:bg-gray-300 dark:shadow-none hover:shadow-lg dark:hover:opacity-70 duration-150"
+                >
+                    Next Post
+                </Link>
             </div>
         </div>
     )
@@ -48,10 +78,43 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
     const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8')
     const { data: frontmatter, content } = matter(fileName)
+    const { postNum: postNum } = frontmatter
+    // const { postNum } = frontmatter
+
+    // get list of files from the posts folder
+    const files = fs.readdirSync('posts')
+
+    // get frontmatter & slug from each post
+    const posts = files.map((fileName) => {
+        const slug = fileName.replace('.md', '')
+        const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8')
+        const { data: frontmatter } = matter(readFile)
+
+        return {
+            slug,
+            frontmatter,
+        }
+    })
+
+    const adjacentPosts = posts.flatMap((post) => {
+        const { slug, frontmatter } = post
+        const {
+            frontmatter: { postNum: pn },
+        } = post
+        if (parseInt(postNum) - 1 == parseInt(pn)) {
+            return slug
+        } else if (parseInt(postNum) + 1 == parseInt(pn)) {
+            return slug
+        } else {
+            return []
+        }
+    })
+
     return {
         props: {
             frontmatter,
             content,
+            adjacentPosts,
         },
     }
 }
